be444ca4f79f07817c00efc880a7ec2baf52d4d8
Alex Lynham <acelynham@hotmail.com>
2018-10-02 23:09:29 +0100

    Ch2 done.

---
+++ ch2-ex3.hs
@@ -0,0 +1,14 @@
+
+xTimesThreePlusY = x * 3 + y
+        where x = 3
+              y = 1000
+
+modifiedXTimesFive = x * 5
+  where y = 10
+        x = 10 * 5 + y
+
+xyz = z / x + y
+  where x = 7
+        y = negate x
+        z = y * 10
+
